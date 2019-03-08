b890440d272d0823329dec4d2cb942461160691e
yesifan <yesifan@duiba.com.cn>
2019-03-08 16:09:30 +0800

    build

---
+++ react/build/precache-manifest.89d4c4caf1be0aaa6d0998dfa5734498.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8f0a396bffc971238127",
+    "url": "/react/build/static/js/runtime~main.0ab4c741.js"
+  },
+  {
+    "revision": "857f00caf6cdb9166284",
+    "url": "/react/build/static/js/main.8dad4bce.chunk.js"
+  },
+  {
+    "revision": "6ea63785b0350922e924",
+    "url": "/react/build/static/js/2.8ba8db20.chunk.js"
+  },
+  {
+    "revision": "857f00caf6cdb9166284",
+    "url": "/react/build/static/css/main.0e518cf4.chunk.css"
+  },
+  {
+    "revision": "d70954296a7afd710d9e54b88a9bbba3",
+    "url": "/react/build/index.html"
+  }
+];
