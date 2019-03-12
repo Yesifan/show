650046fc79de457a5888d3b1eb999a6b3f30c72c
yesifan <yesifan@duiba.com.cn>
2019-03-12 15:45:39 +0800

    build

---
+++ react/build/precache-manifest.2065252b3fdd04ab8590f6861306fe01.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8f0a396bffc971238127",
+    "url": "/react/build/static/js/runtime~main.0ab4c741.js"
+  },
+  {
+    "revision": "fb7e698a44fb4b886e4f",
+    "url": "/react/build/static/js/main.2ee7f162.chunk.js"
+  },
+  {
+    "revision": "c47c47be32394476e25d",
+    "url": "/react/build/static/js/2.1e0edba5.chunk.js"
+  },
+  {
+    "revision": "fb7e698a44fb4b886e4f",
+    "url": "/react/build/static/css/main.b375d2a0.chunk.css"
+  },
+  {
+    "revision": "324cd8f16ef7bdfc70b9fcc882b2f486",
+    "url": "/react/build/index.html"
+  }
+];
--- react/build/service-worker.js
+++ react/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/react/build/precache-manifest.87ab1cb31b7b7088d163bdf415f9fd19.js"
+  "/react/build/precache-manifest.2065252b3fdd04ab8590f6861306fe01.js"
 );
 
 workbox.clientsClaim();
