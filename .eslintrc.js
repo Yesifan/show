62e2425e1db85138bf19a10d2acfa1cd5578df2d
yesifan <yesifan@duiba.com.cn>
2019-03-29 18:17:39 +0800

    ESLINT

---
+++ .eslintrc.js
@@ -0,0 +1,34 @@
+/**
+ * eslint 配置
+ * 0 = off, 1 = warn, 2 = error
+ */
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es6: true
+  },
+  extends: "eslint:recommended",
+  parserOptions: {
+    ecmaFeatures: {
+        "jsx": true
+    },
+    ecmaVersion: 2018,
+    sourceType: "module"
+  },
+  extends:"react-app",
+  rules: {
+    eqeqeq:["warn", "always"],
+    indent: ["error", 2],
+    "linebreak-style": ["error", "windows"],
+    quotes: ["error", "single"],
+    semi: ["error", "always"],
+    "no-trailing-spaces":2,
+    "space-before-blocks":2,
+    "space-before-function-paren":2,
+    "space-infix-ops":2,
+    "space-in-parens":2,
+    "space-unary-ops":2,
+    "spaced-comment":2
+  }
+};
