b4db415033ee6f45ed0f17f3be5c3bde6c898923
yesifan <yesifan@duiba.com.cn>
2019-04-04 16:46:28 +0800

    build

---
+++ react/build/precache-manifest.b352540cc3a3647cf447dd6febfb1369.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c32e58e7c0c7c3c1c8123cd2631eace1",
+    "url": "/react/build/static/media/IndieFlower.c32e58e7.ttf"
+  },
+  {
+    "revision": "8f0a396bffc971238127",
+    "url": "/react/build/static/js/runtime~main.0ab4c741.js"
+  },
+  {
+    "revision": "2d7fdfb4ed1e10f0c99d",
+    "url": "/react/build/static/js/main.04142fd1.chunk.js"
+  },
+  {
+    "revision": "95dccce143a8e8519a93",
+    "url": "/react/build/static/js/2.03d0f84e.chunk.js"
+  },
+  {
+    "revision": "2d7fdfb4ed1e10f0c99d",
+    "url": "/react/build/static/css/main.8e411c86.chunk.css"
+  },
+  {
+    "revision": "2ed92fd2cc020ee133ee0406c2f66f9e",
+    "url": "/react/build/index.html"
+  }
+];
