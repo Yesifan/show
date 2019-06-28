a9ce461599d614c90d4f781298e321bb9d2c0eb6
YeSifan <yesifan66@gmail.com>
2019-06-28 18:01:23 +0800

    Updates

---
+++ raect/precache-manifest.2865cdc9b58dfd6cfe82866c458d466b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c32e58e7c0c7c3c1c8123cd2631eace1",
+    "url": "/react/build/static/media/IndieFlower.c32e58e7.ttf"
+  },
+  {
+    "revision": "5ba9a5a2ae8586ea0c99",
+    "url": "/react/build/static/js/runtime~main.47896f1e.js"
+  },
+  {
+    "revision": "ae630abb67b999b86166",
+    "url": "/react/build/static/js/main.65689ff2.chunk.js"
+  },
+  {
+    "revision": "a7a04ec0ea3dba3aef1d",
+    "url": "/react/build/static/js/2.81ea38e6.chunk.js"
+  },
+  {
+    "revision": "ae630abb67b999b86166",
+    "url": "/react/build/static/css/main.d11b3bab.chunk.css"
+  },
+  {
+    "revision": "880f96c31d1ec437bc938eb3f1efe6c9",
+    "url": "/react/build/index.html"
+  }
+];
